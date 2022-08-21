f8ffdda5f8174144f6ab695fddbb4041d97a59b2
iodeslykos <42@iodeslykos.com>
2022-08-20 20:47:49 -0700

    INIT: We got ourselves a bot. :robot:

---
+++ README.md
@@ -0,0 +1,42 @@
+# SHEA (a.k.a. "Bae")
+
+## Overview.
+
+### The simple heuristic entertainment administrator.
+
+This one's simple: A Discord bot initially dreamt of to serve the needs of the Feature Film Friday Posse. What will it become? No idea. But even if it doesn't grow to contain an exhaustive set of features, we hope it will serve its purpose.
+
+## Installation.
+
+#### A quick note:
+
+Being as this project is wholly written in Python3, we'd recommend installation of Python3. If you do not currently have a Python3 interpreter installed and do not know how to accomplish this, visit [the documentation for Python3](https://wiki.python.org/moin/BeginnersGuide) or call the cops, because you might be in trouble.
+
+Inside of [`requirements.txt`](requirements.txt) you'll find a list of requirements (just as the file name implies).
+
+We recommend creating a virtual environment since--after all--this Python we're dealing with. Stuff breaks.
+
+```
+# python3 -m venv ".venv"
+# pip3 install -r requirements.txt
+```
+
+Before running SHEA for the first time, create a copy of config.json.example, remove the `.example` suffix, and then configure to your liking. You will need to acquire a Discord API key at the very least.
+
+## Features / Roadmap.
+
+- [ ] Spaghetti wolf.
+- [ ] Film selection.
+  - [ ] Create lists of submissions on a per-user basis.
+    - [ ] Allow users to add, remove, and prioritize their selections.
+- [ ] Determine if user is present/allow a "raised hand" to signal participation.
+- [ ] Verbose debug.
+- [ ] Administrative commands and overrides. 
+
+## License.
+
+The contents of this repository are covered under the MIT standard license. See [`LICENSE.md`](LICENSE.md) for details.
+
+## Issues & Contributions.
+
+Should you desire to contribute to or complain about the contents of this repository, feel free to open an Issue or submit a Pull Request. Though we cannot guarantee your satisfaction, requests for support and general praise will be accepted.
---
+++ shea.py
@@ -0,0 +1,99 @@
+"""
+S.H.E.A. (aka Bae)
+
+The simple heuristic entertainment administrator.
+"""
+
+import discord
+from discord.ext import commands
+import random
+from datetime import datetime as time
+import os
+import json
+import logging
+
+# Global variables and logging configuration.
+
+shea_version = "0.0.1"
+
+CONFIG_FILE = open('config.json', 'r')
+CONFIG = json.load(CONFIG_FILE)
+
+BOT_TOKEN = CONFIG['bot_token']
+
+LOG_DATE = time.now()
+LOG_DIR = CONFIG['log_dir']
+LOG_LEVEL = logging.DEBUG
+LOG_FILE = LOG_DATE.strftime(os.path.join(LOG_DIR, "shea-bae_%Y%m%d.log"))
+LOG_TIMESTAMP_FORMAT = CONFIG['timestamp_format']
+
+try:
+    os.makedirs(LOG_DIR, exist_ok=True)
+except TypeError:
+    print("[ERROR]: Unable to create log path.")
+    exit(2)
+
+logger = logging.getLogger('shea_bae')
+logger.setLevel(LOG_LEVEL)
+file_handler = logging.FileHandler(
+    filename=LOG_FILE, encoding='utf-8', mode='a')
+file_handler.setLevel(LOG_LEVEL)
+file_handler.setFormatter(logging.Formatter(
+    '%(asctime)s - %(name)s - :[%(levelname)s] %(message)s', datefmt=LOG_TIMESTAMP_FORMAT))
+logger.addHandler(file_handler)
+
+
+########################################################################################################################
+
+intents = discord.Intents.default()
+intents.message_content = True
+bae = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=intents)
+
+
+@bae.event
+async def on_ready(self):
+    print(f"Logged in as {self.user} (ID: {self.user.id}")
+    logger.info("Logged in as {self.user} (ID: {self.user.id}")
+
+
+@bae.event
+async def on_error(self):
+    print(f"%s", self)
+    logger.fatal("%s", self)
+    exit(1)
+
+
+@bae.command(name="roll")
+async def dice_roll(self, n: int, x: int):
+    self.roll_results = []
+    for i in range(n - 1):
+        roll_result = random.randrange(x)
+        self.roll_results.append(roll_result)
+    await self.send(self.roll_results)
+
+
+@bae.command(name="spaghetti_wolf")
+async def spaghetti_wolf(self):
+    await self.send("`insert spaghetti wolf here`")
+
+
+@bae.command(name="ping")
+async def ping(self):
+    await self.send("Received ping from {self.author.mention}. Ack?")
+
+
+########################################################################################################################
+
+description = """
+SHEA: The simple heuristic entertainment administrator.
+
+A Discord bot designed to help with the alignment and selection of films for the Feature Film Friday Posse of the Shea \
+Discord Guild.
+"""
+
+# Commence.
+
+try:
+    bae.run(BOT_TOKEN)
+except Exception as e:
+    logger.fatal(e)

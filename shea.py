aa46ce10aeb55fc7635770bd80dcba624053237e
iodeslykos <42@iodeslykos.com>
2022-08-22 11:57:30 -0700

    Add banner and startup logging for fun + debug

--- shea.py
+++ shea.py
@@ -14,11 +14,25 @@ import sys
 import json
 import logging
 
-shea_version = "0.0.6"
 
 ########################################################################################################################
 # Configuration.
 ########################################################################################################################
+
+BOT_VERSION = "0.0.7"
+BOT_BANNER = (f"""  _________ ___ ______________   _____   
+ /   _____//   |   \\_   _____/  /  _  \\  
+ \\_____  \\/    ~    \\    __)_  /  /_\\  \\ 
+ /        \\    Y    /        \\/    |    \\
+/_______  /\\___|_  /_______  /\\____|__  /
+        \\/       \\/        \\/         \\/
+Simple Heuristic Entertainment Administrator 
+v{BOT_VERSION}
+""")
+
+#Commence!
+
+print(f"{BOT_BANNER}")
 
 CONFIG = {}
 CONFIG_PATH = 'config.json'
@@ -26,11 +40,13 @@ CONFIG_PATH = 'config.json'
 try:
     CONFIG_FILE = open(CONFIG_PATH, 'r')
     CONFIG = json.load(CONFIG_FILE)
+    print(f"[INFO]: Loaded configuration file: \'{CONFIG_PATH}\'")
 except FileNotFoundError:
     print(f"[ERROR] Unable to load configuration file: \'{CONFIG_PATH}\'")
     exit(1)
 
 BOT_TOKEN = CONFIG['bot_token']
+BOT_NAME = CONFIG['bot_name']
 
 LOG_DATE = time.now()
 LOG_DIR = CONFIG['log_dir']
@@ -39,37 +55,50 @@ LOG_FILE = LOG_DATE.strftime(os.path.join(LOG_DIR, "shea-bae_%Y%m%d.log"))
 LOG_TIMESTAMP_FORMAT = CONFIG['timestamp_format']
 
 try:
-    os.makedirs(LOG_DIR, exist_ok=True)
+    if os.path.exists(LOG_DIR):
+        print(f"[INFO]: Log directory already exists: {LOG_DIR}")
+    else:
+        print(f"[INFO]: Log directory does not exist. {LOG_DIR} Creating...")
+        os.makedirs(LOG_DIR, exist_ok=True)
 except TypeError:
     print(f"[ERROR]: Unable to create log path: {LOG_DIR}")
     exit(2)
 
-logger = logging.getLogger('shea_bae')
-logger.setLevel(LOG_LEVEL)
-logger_format = logging.Formatter('%(asctime)s [%(levelname)s] %(funcName)s: %(message)s', datefmt=LOG_TIMESTAMP_FORMAT)
-file_handler = logging.FileHandler(
-    filename=LOG_FILE, encoding='utf-8', mode='a')
-file_handler.setLevel(LOG_LEVEL)
-file_handler.setFormatter(logger_format)
-logger.addHandler(file_handler)
-
-if CONFIG['log_stdout']:
-    print(f"[INFO]: Enabling logging to console.")
+try:
+    print(f"[INFO]: Configuring logging.")
+    logger = logging.getLogger('main')
+    logger.setLevel(LOG_LEVEL)
+    logger_format = logging.Formatter('%(asctime)s [%(levelname)s] %(funcName)s: %(message)s', datefmt=LOG_TIMESTAMP_FORMAT)
+    file_handler = logging.FileHandler(
+        filename=LOG_FILE, encoding='utf-8', mode='a')
+    file_handler.setLevel(LOG_LEVEL)
+    file_handler.setFormatter(logger_format)
+    logger.addHandler(file_handler)
+    if CONFIG['log_stdout']:
+        print(f"[INFO]: Enabling logging to console.")
     stream_handler = logging.StreamHandler()
     stream_handler.setFormatter(logger_format)
     logger.addHandler(stream_handler)
+except Exception as log_config_failure:
+    print(f"[INFO]: Failed to configure logging.", log_config_failure)
 
 MEDIA_DIR = CONFIG['media_dir']
 
 for directory in MEDIA_DIR:
     try:
-        os.makedirs(MEDIA_DIR[directory], exist_ok=True)
+        if os.path.exists(MEDIA_DIR[directory]):
+            print(f"[INFO]: Media directory already exists: {MEDIA_DIR[directory]}")
+        else:
+            print(f"[INFO]: Media directory does not exist. {MEDIA_DIR[directory]} Creating...")
+            os.makedirs(MEDIA_DIR[directory], exist_ok=True)
     except TypeError:
         print(f"[ERROR] Unable to create media directory: {MEDIA_DIR[directory]}")
         exit(1)
 
 
 ########################################################################################################################
+
+print(f"[INFO]: Initializing SHEA as {BOT_NAME}")
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -83,7 +112,7 @@ bae = commands.Bot(command_prefix=commands.when_mentioned_or("!"), intents=inten
 
 @bae.event
 async def on_ready():
-    print(f"Logged in as {bae.user} (ID: {bae.user.id})")
+    print(f"[ OK ]: Logged in as {bae.user} (ID: {bae.user.id})")
     logger.info(f"Logged in as {bae.user} (ID: {bae.user.id})")
 
 
@@ -221,6 +250,7 @@ async def shea_help(self):
 ########################################################################################################################
 
 try:
+    print(f"[INFO]: Attempting login to Discord.")
     bae.run(BOT_TOKEN)
 except Exception as e:
     logger.fatal(e)

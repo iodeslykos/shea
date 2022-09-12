2e4437bdafe848cc220daba6c3e00152e6c75eb3
iodeslykos <42@iodeslykos.com>
2022-09-12 14:42:12 -0700

    Move install-venv.sh -> scripts/setup-venv.sh

--- scripts/setup-venv.sh
+++ scripts/setup-venv.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 ###
-# { install-venv.sh } -- A helper script to quickly create and configure a Python3 virtual environment.
+# { setup-venv.sh } -- A helper script to quickly create and configure a Python3 virtual environment.
 ###
 
-version="0.1.3"
+version="0.1.4"
 
-py_name="SHEA"
+py_name=$1
+install_default=$3
 install_venv="y"
 install_latest_pycord="n"
 venv_path=".venv"
+
+if [ -n "$2" ]; then
+  venv_path=$2
+fi
 
 # Output tags.
 tag_banner="{ install-venv.sh } v$version"
@@ -43,8 +48,8 @@ elif [ -d "$venv_path" ]; then
   echo ""
   exit 1
 else
-  echo "$tag_okay No virtual environment exists at '$(pwd)$venv_path'!"
-  while true; do
+  echo "$tag_okay No virtual environment exists at '$(pwd)/$venv_path'!"
+  while [ "$install_default" != 'true' ]; do
     read -rp "[ ?? ] Create a new virtual environment? Y/n " prompt
     case $prompt in
       [Yy]) install_venv="y";
@@ -55,7 +60,7 @@ else
       *) echo "$tag_warn Invalid entry! Please enter [y]es or [n]o."
     esac
   done
-  while true; do
+  while [ "$install_default" != 'true' ]; do
     read -rp "[ ?? ] Install latest version of Py-cord? (for testing) y/N " prompt
     case $prompt in
       [Yy])
@@ -75,7 +80,7 @@ fi
 if [ $install_venv = "y" ]; then
   echo "$tag_info Creating virtual environment!"
   python3 -m venv "$venv_path"
-  . $venv_path/bin/activate
+  . "$venv_path"/bin/activate
   echo "$tag_okay Created virtual environment \"$(pwd)/$venv_path\"!"
   if [ "$(which python3)" != "$(pwd)/$venv_path/bin/python3" ]; then
     echo "$tag_fail Failed to create and activate virtual environment!"
@@ -84,7 +89,7 @@ if [ $install_venv = "y" ]; then
     echo "$tag_okay Successfully created and activated virtual environment!"
   fi
   echo "$tag_info Upgrading pip!"
-  if ! $venv_path/bin/python3 -m pip install pip --upgrade 1> /dev/null 2>&1; then
+  if ! "$venv_path"/bin/python3 -m pip install pip --upgrade; then
     echo "$tag_fail Failed to upgrade pip!"
     exit 2
   else
@@ -95,7 +100,7 @@ if [ $install_venv = "y" ]; then
   else
     echo "$tag_fail Failed to find 'requirements.txt'!"
   fi
-  if ! $venv_path/bin/pip3 install -r requirements.txt 1> /dev/null 2>&1; then
+  if ! "$venv_path"/bin/pip3 install -r requirements.txt; then
     echo "$tag_fail Failed to install required modules!"
     exit 2
   else
@@ -105,7 +110,7 @@ if [ $install_venv = "y" ]; then
   # For installation of latest version of Py-cord.
   if [ $install_latest_pycord = "y" ]; then
     echo "$tag_info Installing latest version of Py-cord!"
-    if $venv_path/bin/pip3 install -U git+https://github.com/Pycord-Development/pycord 1> /dev/null 2>&1; then
+    if "$venv_path"/bin/pip3 install -U git+https://github.com/Pycord-Development/pycord; then
       echo "$tag_fail Failed to upgrade to latest version of Py-cord!"
       echo "    NOTE: This may be fine if current version of Py-cord>=2.1.0."
     else
@@ -116,9 +121,9 @@ if [ $install_venv = "y" ]; then
   # Point of completion.
   echo "$tag_okay Successfully installed virtual environment!"
   echo "$tag_done Successfully created virtual environment for $py_name!"
-  echo "$tag_info You can attempt to activate the virtual environment with:"
+  echo "$tag_info You can now activate the virtual environment with:"
   echo ""
-  echo "      source "$venv_path/bin/activate""
+  echo "      source $venv_path/bin/activate"
   echo ""
 fi
 

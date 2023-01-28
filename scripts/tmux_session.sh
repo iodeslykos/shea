f5cc69ebd1f53570f500a376cafd744dad3a5b1d
iodeslykos <42@iodeslykos.com>
2023-01-28 06:39:00 +0000

    Better output for tmux_session.sh

--- scripts/tmux_session.sh
+++ scripts/tmux_session.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 ###
-# { tmux_session.sh } -- Create a tmux session running SHEA!
+# { tmux_session.sh } -- Create a tmux session running SHEA.
 ###
 
 # This script is intended to run on boot to ensure SHEA persists through automated system updates.
@@ -10,16 +10,19 @@ session="shea"
 directory="$HOME/shea"
 
 if /usr/bin/tmux has-session -t $session > /dev/null 2>&1; then
-  echo "[ FAIL ] Session $session already running!"
+  echo "[FAIL] tmux session \"$session\" already running!"
   exit 1
 else
-  echo "[ INFO ] Starting $session."
+  echo "[INFO] Starting tmux session \"$session\"."
   /usr/bin/tmux new-session -d -s $session
   /usr/bin/tmux send-keys -t $session "cd $directory" C-m
   /usr/bin/tmux send-keys -t $session "make run" C-m
   if [ $? -eq 0 ]; then
-    echo "[  OK  ] $session started successfully."
+    echo "[ OK ] Session \"$session\" started successfully."
+    echo -e "[INFO] To connect to the session run:\n\n    > tmux attach-session -t $session\n"
   else
-    echo "[ FAIL ] $session failed to start!"
+    echo "[FAIL] tmux session \"$session\" failed to start!"
+    exit 2
   fi
 fi
+
